d035e342a3d24a7b7a26642a9cab125ab5491ae6
ChaoLiu <62862620+bruceliuhi@users.noreply.github.com>
2023-12-19 16:20:05 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
                 .linkedLibrary("z"),
                 .linkedLibrary("bz2"),
                 .linkedLibrary("sqlite3"),
-                .linkedFramework("CoreML")
+                .linkedFramework("CoreML"),
                 .linkedFramework("SystemConfiguration")
             ]
         )
